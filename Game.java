2e09ea942cbe2d92f2bde732c25b94c2d6097e6a
Pierre Hernandez <pierrevincenthernandez@gmail.com>
2021-08-28 18:50:12 +0800

    Add RandomListener

--- Game.java
+++ Game.java
@@ -82,6 +82,52 @@ public class Game {
         
     }
 
+
+    private class RandomListener implements MouseInputListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+        
+    }
     
 
     /*
